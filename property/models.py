749a95c996119ae64d9c13c5d8618f8cb98f4ab1
AlmazNuriakhmetov <aboohnifa@gmail.com>
2023-05-18 15:57:11 +0300

    fill Flat.owner_pure_phone field with normalized phone numbers

--- property/models.py
+++ property/models.py
@@ -7,6 +7,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
+                                        blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -54,9 +56,6 @@ class Flat(models.Model):
     liked_by = models.ManyToManyField(User, related_name='liked_flats',
                                       verbose_name='Кто лайкнул',
                                       blank=True)
-
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
-                                        blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
