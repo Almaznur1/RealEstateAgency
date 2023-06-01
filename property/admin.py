1224e719e2359d4a9ccdff152c5e1471aca80eec
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-06-01 11:12:31 +0300

    registration of models in UI using decorators

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class OwnerInline(admin.TabularInline):
     raw_id_fields = ('owner',)
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'town_district', 'address',)
     readonly_fields = ('created_at',)
@@ -19,14 +20,11 @@ class FlatAdmin(admin.ModelAdmin):
     inlines = [OwnerInline, ]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('about_flat', 'from_whom',)
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
