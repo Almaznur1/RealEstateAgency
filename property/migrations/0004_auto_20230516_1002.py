142c586d170bac99bdf1074c3be70e2d30d785a5
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-06-01 12:22:34 +0300

    Update 0004_auto_20230516_1002.py

--- property/migrations/0004_auto_20230516_1002.py
+++ property/migrations/0004_auto_20230516_1002.py
@@ -5,7 +5,7 @@ from django.db import migrations
 
 def fill_new_building_field(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    Flat.objects.filter(construction_year__gt=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__gt=2014).update(new_building=True)
 
 
 class Migration(migrations.Migration):
